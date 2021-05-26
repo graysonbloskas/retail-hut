98b88f2efb4967963891a412adfb63a35daff808
T Heier <trentheier@gmail.com>
2021-05-26 11:46:44 -0400

    added a basic card for testing, fixed errors from appjs and indexjs

---
+++ client/src/components/CardBody/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CardBody() {
+  return (
+    <div>
+      <h4>Hi this is a test!</h4>
+    </div>
+  );
+}
+
+export default CardBody;
---
+++ client/src/components/CardHeading/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardTitle from "../CardTitle";
+
+function CardHeading() {
+  return ( 
+    <div>
+      <CardTitle />
+    </div> 
+  );
+}
+
+export default CardHeading;
