8448e1118a919b1d8f5e604c048fa054e83c2ac9
Grayson Bloskas <graysonbloskas@gmail.com>
2021-05-26 14:08:15 -0400

    uploading header and associated components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
 import Cards from "../src/components/Cards"
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Cards />
     </div>
   );
---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../../assets/images/retail-hut-logo.png'
+import './logo.css'
+
+function Logo() {
+    return (
+        <div>
+            <img src={logo} alt="Logo" className="logo" />
+        </div>
+    )
+}
+
+export default Logo
