ea18c8b98691fd5031d0804974b2793468b86c45
T Heier <trentheier@gmail.com>
2021-05-28 10:41:48 -0400

    created a very basic footer that looks super horrible so someone please fix it and make it look nicer i don't know how to do front end make things nice

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import Container from '../../components/Container';
-import Col from '../../components/Col';
-import Row from '../../components/Row';
-import homepageHero from '../../components/Homepage-Hero';
-import Header from '../../components/Header'
-import FeaturedList from '../../components/FeaturedList';
-import Nav from '../../components/Nav'
+import React from "react";
+import Container from "../../components/Container";
+import Col from "../../components/Col";
+import Row from "../../components/Row";
+import homepageHero from "../../components/Homepage-Hero";
+import Header from "../../components/Header";
+import FeaturedList from "../../components/FeaturedList";
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
+
 const Homepage = () => {
-    return (
-        <Container fluid>
-            <Row>
-                <Col size='md-6'>
-                    <Header />
-                    <Nav />
-                </Col>
-            </Row>
-            <Row>
-            <FeaturedList />
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-6">
+          <Header />
+          <Nav />
+        </Col>
+      </Row>
+      <Row>
+        <FeaturedList />
+      </Row>
+      <Col>
+        <Row>
+          <Footer />
+        </Row>
+      </Col>
+    </Container>
+  );
+};
 
 export default Homepage;
