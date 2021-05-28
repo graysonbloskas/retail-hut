efe08973aef572c18c1e96a117aeadac037b8bb9
T Heier <trentheier@gmail.com>
2021-05-27 20:25:54 -0400

    created very basic staff picks component. we still need to create a function the dynamically create cards with their item data

---
+++ client/src/components/StaffPicksList/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from "../Container"
+import Row from "../Row"
+import Col from "../Col"
+import Card from "../Card"
+
+function StaffPicks() {
+    <div>
+        <Container>
+            <Row>
+                <Col>
+                <h1>This is the staff picks list</h1>
+                <Card />
+                <Card />
+                </Col>
+            </Row>
+        </Container>
+    </div>
+}
