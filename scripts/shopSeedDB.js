46513376e6c16dc8ea6250e625da4a9597390191
Brandi Murray <pritchettb14@gmail.com>
2021-05-30 17:17:46 -0400

    ALL SEEDS WORKINGgit add -A! base data added will add more... can run npm run seed

--- scripts/shopSeedDB.js
+++ scripts/shopSeedDB.js
@@ -1,56 +1,77 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
+ function shopInit (){
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/retailhut");
 
 let shopSeed = [
   {
     name: "TeeLikeYours",
     href: "https://www.etsy.com/shop/TeeLikeYours?ref=simple-shop-header-name&listing_id=551742682",
-    type: ["Yellow Tee Shirt", "Red Dress", "Blue Tee"],
+    type: ["Yellow Tee Shirt", "Red Dress", "Blue Tee", "Navy Blue Tee","Pink Women's Tee"],
     products: []
   },
   {
     name: "BrielleShoppe",
     href: "https://www.etsy.com/shop/BrielleShoppe?ref=yr_purchases",
-    type: [],
-    products:[]
+    type: ["Makeup Brushes", "Makeup Sponge"],
+    products: []
   },
   {
     name: "FancyLittleGiftsCo",
     href: "https://www.etsy.com/shop/FancyLittleGiftsCo?ref=simple-shop-header-name&listing_id=751831474",
-    type: ["Men's Wristwatch"],
-    products:[]
+    type: ["Men's Wristwatch", "Women's Wrist Watch", "Father's Day Tee" ],
+    products: []
   },
   {
     name: "EclipseGamingSupply",
     href: "https://www.etsy.com/shop/EclipseGamingSupply?ref=simple-shop-header-name&listing_id=810175125",
-    type: ["PS4 Controller"],
-    products:[]
+    type: ["PS4 Controller", "PS4 Controller Custom", "Super Mario Maker 2", "Xbox controller", "Gaming Keyboard"],
+    products: []
 
   }
 ];
 
-db.Product.find().then(productData =>{ 
+ db.Product.find().then(productData => {
   shopSeed = shopSeed.map(shop => {
-    productData.forEach(product => {
-      switch (product.title){
-        case "Yellow Tee Shirt":
-          case "Red Dress":     
-          case "Blue Tee":
-shop.products.push(product._id)
-          break 
-      }
+    shop.type.forEach(type => {
+       console.log(type)
+      productData.forEach(product => {
+        console.log(type, product.title)
+        if (type === product.title) {
+          shop.products.push(product._id)
+        }
+      })
     })
-    return shop
+    return {
+      name: shop.name,
+      href: shop.href,
+      products:shop.products 
+    }
   })
 
 
   db.Shop
   .remove({})
   .then(() => db.Shop.collection.insertMany(shopSeed))
-
+.then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+      })
+      .catch(err => {
+        console.error(err);
+        process.exit(1);
+      });
 })
+
+}
+
+
+
+ 
+
+shopInit()
 
 // 
 
