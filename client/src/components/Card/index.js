823bab3e6419e8d88e2058cde64814f129d13b04
T Heier <trentheier@gmail.com>
2021-06-01 19:00:16 -0400

    deleted all the card components. thew new Card component is now a bootstrap card with no styling

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import CardBody from "../CardBody";
-import CardBtn from "../CardBtn";
-import CardImg from "../CardImage";
-import CardHeading from "../CardHeading";
 
 function Card() {
   return (
-    <div className="card">
-      <CardHeading />
-      <CardImg />
-      <CardBody />
-      <CardBtn style={{ opacity: 1 }} data-value="back" />
-      <CardBtn style={{ opacity: 1 }} data-value="next" />
+    <div className="card" style={{ width: "18rem" }} key={i}>
+      <img className="card-img-top" src={product.image} alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{product.title}</h5>
+        <p className="description">{product.description}</p>
+        <p className="card-text">{product.price}</p>
+        <a href="#" className="btn btn-primary">
+          Add to Cart
+        </a>
+      </div>
     </div>
   );
 }
