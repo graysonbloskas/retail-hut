8888b5a21568a0bdec3420d5cdaa2e1792856462
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-01 22:34:45 -0400

    fixed card styling. Looks banging

--- client/src/components/Card/element.js
+++ client/src/components/Card/element.js
@@ -1,34 +1,6 @@
 import styled from "styled-components";
 
-export const CardContainer = styled.div`
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: #ffffff;
-  padding-bottom: 50px;
-  padding-top: 50px;
-  padding-left: 8%;
-  padding-right: 8%;
-`;
 
-export const CardWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  align-items: center;
-  grid-gap: 40px;
-  /* padding: 0 50px; */
-
-  @media screen and (max-width: 1300px) {
-    grid-template-columns: 1fr 1fr;
-  }
-
-  @media screen and (max-width: 875px) {
-    grid-template-columns: 1fr;
-    padding: 0 20px;
-  }
-`;
 export const ProductCard = styled.div`
   background: #ffffff;
   display: flex;
